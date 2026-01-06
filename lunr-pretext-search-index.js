d9742c94efc86163d9a8e585b2b279023060fe31
blake-farman <blake.farman@gmail.com>
2026-01-06 16:39:23 -0600

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,104 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "frontmatter",
+  "level": "1",
+  "url": "frontmatter.html",
+  "type": "Front Matter",
+  "number": "",
+  "title": "Front Matter",
+  "body": "  Blake Farman Mathematics and Statistics Louisiana Tech University        copyright   "
+},
+{
+  "id": "contact_information",
+  "level": "1",
+  "url": "contact_information.html",
+  "type": "Section",
+  "number": "1",
+  "title": "Contact Information",
+  "body": " Contact Information      Professor  Dr. Blake Farman    Phone Number  318.257.2951    Email Address   bfarman@latech.edu    Website      Office  Nethken 235       Office Hours     Mon\/Wed\/Fri  12:30 PM  3:00 PM    Tues\/Thurs  1:00 PM  2:15 PM       Preferred Method of Communication  The best way to communicate with me during the semester is through email. I monitor my email during regular business hours and try to respond within one business day.    Official University Email Addresses  The University provides each student with an email address and all official course correspondence will only be conducted using official university email addresses.    Email Etiquette  When you send an email, your message should contain the following information   Subject  A few words that describe the content of your email along with the course identifier (Math 308 001)    Salutation  A professional greeting such as Dear or Hello followed by the appropriate honorific and the recipient's last name (  Dr. X).   If the person you are addressing holds a doctorate ( a PhD ), then you should address the recipient as Dr. X, never Mr. X, Ms. X, or Mrs. . X.    If you are unsure, Professor X is generally a safe way to address a faculty member.       Body  A concise message that clearly expresses the purpose of your email.    Signature  A simple closing (e.g. Sincerely,  Thank you,  Best regards,  ) followed by your first and last name.   For your convenience, included below is an example email.   Sample Email  Subject: Meeting - Math 308 001  Dear Dr. Farman,  I am having trouble with this week's homework. I have tried working through the examples in the text, but I am struggling to understand the concepts involved. Could we schedule a time to meet before the next class to discuss some of the concepts in more detail?  Thank you,  Jane Doe    "
+},
+{
+  "id": "course_information",
+  "level": "1",
+  "url": "course_information.html",
+  "type": "Section",
+  "number": "2",
+  "title": "Course Information",
+  "body": " Course Information   Course Description  Matrices, systems of linear equations, vectors, vector spaces, linear transformations, eigenvalues and eigenvectors.    Course Prerequisites  A grade of C or better in MATH 243 or MATH 311.    Student Learning Outcomes  The student will demonstrate proficiency the ideas and techniques of Linear Algebra by performing adequately on a series of exams and homework exercises.    Course Topics  Topics include   The Geometry and Algebra of     Systems of Linear Equations    Spanning sets and Linear Independence    Matrix Operations    Matrix Algebra    The LU Factorization    Subspaces, Basis, Dimension, and Rank    Linear Transformations    Eigenvalues and Eigenvectors    Determinants       Instructional Methods  This course is offered as a face-to-face course. Learning will be facilitated through traditional lecture, group work\/activities, homework, and in-class assessments.   "
+},
+{
+  "id": "evaluation",
+  "level": "1",
+  "url": "evaluation.html",
+  "type": "Section",
+  "number": "3",
+  "title": "Evaluation",
+  "body": " Evaluation   This course will use Standards Based Grading . The content is broken into standards that you are expected to master by the end of the course.  This grading system is iterative in the sense that you will have multiple opportunities to display mastery of each standard, and forgetful in the sense that your previous unsuccessful attempts are discarded once you demonstrate mastery of a standard.  Mastering a standard is a two-step process. You will need to earn a score of M on the Homework that goes with the standard and an in-class Assessment.  A description of each standard can be found in the section.    Assessments  This course will have in-class, written assessments. You may not use the text, your notes, or any devices.  Each assessment will contain problems corresponding to one or more standards. The standards are scored independently using the scale in the section. To master a standard, you must earn a score of M on all of the problems that go with the standard.   Reassessment  There are scheduled Reassessment days for this course (see the ) where you will be given problems for every standard that you have not yet mastered . You may attempt as many standards as time allows.     Homework  Homework will be assigned regularly. The problems are chosen to highlight the core concepts from the standards. Mastery of these homework sets serves as a good indicator for performance on assessments. As such, you should enure that you fully understand the material on these homework sets; that is, upon completion of the homework set, you should be capable of completing similar problems without the aid of the text, a computer, or any other tools not available during an exam.   Reassessment  You may resubmit one homework set that you have not mastered each week. You only need to correct and resubmit the exercises with mistakes.     Scoring  All written work in this course will be scored on the following scale.   Scale     Mastery (M)  The given solution is correct with no content related errors. Appropriate justification is provided in a clear, easy to follow manner.    Progressing (P)  The given solution demonstrates an understanding of the material, but contains content related errors or lacks justification.    Needs Improvement (N)  The given solution was blank, illegible, or used inappropriate techniques.        Grading Scale  Letter grades will be assigned based on the number of standards mastered in the course, as specified in the table below.   Grading Scale    Letter Grade  Number of Standards Mastered    A  17 18    B  15 16    C  13 14    D  11 12    F  0-10      "
+},
+{
+  "id": "grading_scale-3",
+  "level": "2",
+  "url": "evaluation.html#grading_scale-3",
+  "type": "Table",
+  "number": "3.1",
+  "title": "Grading Scale",
+  "body": " Grading Scale    Letter Grade  Number of Standards Mastered    A  17 18    B  15 16    C  13 14    D  11 12    F  0-10    "
+},
+{
+  "id": "standards",
+  "level": "1",
+  "url": "standards.html",
+  "type": "Section",
+  "number": "4",
+  "title": "Standards",
+  "body": " Standards    Systems of Equations (SE)   Systems of Equations 1   I can..     identify whether or not a matrix is in Reduced Row Echelon Form,    use Gaussian Elimination to put a matrix into Reduced Row Echelon Form,    use the Reduced Row Echelon Form of an augmented matrix to describe the solution space to a system of linear equations using appropriate notation.      Homework:  Section 1.2 , Exercises 1 2.     Systems of Equations 2   I can..     use Pivot Positions to determine whether a linear system is consistent,  determine whether the solution to a consistent linear system is unique.     Homework:  Section 1.4 , Exercises 1 3.      Vectors and Matrices (VM)   Vectors and Matrices 1   I can..     add vectors,    scale vectors by a real number,    determine whether a vector can be expressed as a linear combination of a set of given vectors.      Homework:  Section 2.1 , Exercises 1 3, 5.     Vectors and Matrices 2   I can..     add matrices,    scale matrices by a real number,    perform matrix-vector multiplication,    perform matrix-matrix multiplication,    translate a system of equations into a matrix equation of the form .      Homework: Section Section 2.2 , Exercises 1 6.     Vectors and Matrices 3   I can..     determine whether a vector is in the span of a given set of vectors,    describe the span of a set of vectors as a set,    use the span to determine whether a linear system is consistent.      Homework: Section Section 2.3 ,Exercises 1 5, 10.     Vectors and Matrices 4   I can..     I can determine whether a given set of vectors is linearly independent.      Homework: Section Section 2.4 ,Exercises 1 2, 5.     Vectors and Matrices 5   I can..     define a matrix transformation,    find the domain and range of a matrix transformation,    compose matrix transformations,    determine whether a given function is a matrix transformation.      Homework: Section Section 2.5 , Exercises 1 4.      Invertibility and Bases (IB)   Invertibility and Bases 1   I can..     use Gaussian Elimination to determine whether a matrix is invertible,    find the inverse of an invertible matrix.      Homework:  Section 3.1 , Exercises 1, 3, 4.     Invertibility and Bases 2   I can..     find a basis for a space,    translate between the coordinate systems for different bases.      Homework:  Section 3.2 , Exercises 1 4.     Invertibility and Bases 3   I can..     compute the determinant of a given matrix,    use the determinant to determine whether a matrix is invertible.      Homework:  Section 3.4 , Exercises 1, 3 4, 6.     Invertibility and Bases 4   I can..     find a basis for the null space of a given matrix,    find a basis for the column space of a given matrix,    determine whether a given set satisfies the definition of a vector space.      Homework: Section 3.5 , Exercises 1 2, 5.      Eigentheory (ET)   Eigentheory 1   I can..     determine whether a vector is an eigenvector of a given matrix,    find the eigenvalue associated to an eigenvector.      Homework:  Section 4.1 , Exercises 1 2, 5 6.     Eigentheory 2   I can..     use the characteristic polynomial to find the eigenvalues of a given matrix,    find the algebraic and geometric multiplicities of eigenvalues,    find a basis for the eigenspace associated to an eigenvalue.      Homework:  Section 4.2 , Exercises 1 2, 5.     Eigentheory 3   I can..     determine whether a given matrix is diagonalizable,    diagonalize a diagonalizable matrix.      Homework:  Section 4.3 , Exercises 1 2, 7.      Orthogonality (OR)   Orthogonality 1   I can..     compute the dot product of two vectors,    use the dot product to find the angle between two vectors,    determine whether two vectors are orthogonal.      Homework:  Section 6.1 , Exercises 1 4.     Orthogonality 2   I can..     compute the transpose of a matrix,    use the transpose to find a basis for the orthogonal complement of a given vector space.      Homework:  Section 6.2 , Exercises 1 3.     Orthogonality 3   I can..     I can perform Orthogonal Decomposition.      Homework:  Section 6.3 , Exercises 1 6.     Orthogonality 4   I can..     use Gram-Schmidt Orthogonalization to find an orthogonal basis for a given vector space,    find an orthonormal basis from an orthogonal basis for a given vector space.      Homework:  Section 6.4 , Exercises 1 5.     "
+},
+{
+  "id": "policies",
+  "level": "1",
+  "url": "policies.html",
+  "type": "Section",
+  "number": "5",
+  "title": "Course Policies and Procedures",
+  "body": " Course Policies and Procedures   At a minimum, all policies stated in the current Louisiana Tech University Student Handbook should be followed. Additional class policies follow.    Textbook   Required Textbook  The required text for this course is Understanding Linear Algebra by David Austin. This textbook is freely available in HTML format and in PDF format. If you so desire, you may also obtain a low-cost print version.      Attendance Policy  Per University Policy 2206 :    All students are expected to attend classes in which they are enrolled regularly and punctually. Failure to do so may jeopardize a student's scholastic standing and may lead to suspension from the College or University.  The student is responsible for making arrangements satisfactory to the instructor regarding absences including prior notification of planned absences, providing documentation if needed, and making up of examinations and work as required. For other emergency absences, the student shall submit excuses for class absences to the appropriate instructor within three class days following the student's return to their respective class.    As noted above, students are obligated to complete all assigned work promptly, to attend class regularly, and to participate in whatever class discussion may occur. Any behavior that interferes with the ability of other students to learn is unacceptable. If any of these become an issue, you will be asked to leave class for the day.  Your phone should be on silent and put away during class. Headphones should also be put away during class. Texting and browsing social media during class is rude and disrespectful. Please refrain from using your phone during class unless explicitly stated otherwise. If you are caught with a cell phone on your desk, in your lap, or anywhere in view while taking a quiz or exam, it will be treated as a case of academic dishonesty. Smart watches are also prohibited during exams; please put them away during those times.    Late Policy  Late work will not be accepted. Students who cannot submit work due to a University-related activity requiring an absence from class must arrange for work to be submitted before leaving.    Make-up Policy  In the event of a missed assessment due to absence, students will be given the opportunity to attempt all missed standards on the next reassessment. The date and time of the final reassessment are determined by the registrar and cannot be made up.    Academic Honor Code  In accordance with the Academic Honor Code , students pledge the following:    Being a student of higher standards, I pledge to embody the principles of academic integrity.    Any suspected case of academic dishonesty will be reported. If it is determined that academic misconduct has occurred, the student will receive a grade of F for the course and may be dismised from the University.   "
+},
+{
+  "id": "student_services",
+  "level": "1",
+  "url": "student_services.html",
+  "type": "Section",
+  "number": "6",
+  "title": "Student Services",
+  "body": " Student Services   Accommodations for Students with Disabilities  Students needing testing or classroom accommodations based on a disability must be registered with the Louisiana Tech University Office of Testing & Disability Services. Students are encouraged to discuss those needs with the instructor as soon as possible. If you do not have a current accommodation memo, visit the Office of Testing & Disability Services for assistance.    Counseling Services  Students have access confidential counseling services at no additional cost. Visit the Louisiana Tech Counseling Services website to learn more.    Non-Discrimination  Louisiana Tech University is committed to the principle of providing the opportunity for learning and development of all qualified citizens without regard to race, color, national origin, religion, age, sex, sexual orientation, marital status, or disability. Persons with concerns may contact the Louisiana Tech Director of Title IX Compliance:        Mortissa D. Harvey    Phone  318.257.5911    Office  Wyly Tower 1536    Email  mharvey@latech.edu      Section 504 is a federal law designed to protect the rights of individuals with disabilities in programs and activities that receive Federal financial assistance from the U.S. Department of Education. Persons with concerns may contact the Section 504 Coordinator:        Annie Jantz    Phone:  318.257.2445    Office:  Division of Student Affairs; Keeny Hall 305    Email:  ajantz@latech.edu        Emergency Notification System ( ENS )  All Louisiana Tech students are strongly encouraged to enroll and update their contact information in the Emergency Notification System. It takes just a few seconds to ensure you’re able to receive important text and voice alerts in the event of a campus emergency.    Drop with a W date  The latest date a student may drop a course (or resign his\/her entire schedule) with a W grade for Winter 2024 25 is Friday, February 7, 2025.    Canvas and Workday Help  Direct questions\/problems to the Computing Center Help Desk ( helpdesk@latech.edu or 318-257-5300).   "
+},
+{
+  "id": "schedule",
+  "level": "1",
+  "url": "schedule.html",
+  "type": "Section",
+  "number": "7",
+  "title": "Schedule",
+  "body": " Schedule    December 2 December 6         12\/2  12\/3  12\/4  12\/5  12\/6             1.1 What we can expect         1.2 Finding solutions to linear systems        December 9 December 13         12\/9  12\/10  12\/11  12\/12  12\/13      1.3 Computations with Sage          1.4 Pivots and their influence on solution spaces          2.1 Vectors and Linear Combinations        December 16 December 20            12\/16  12\/17  12\/18  12\/19  12\/20      2.2 Matrix multiplication and linear combinations         2.2 Matrix multiplication and linear combinations  2.3 The span of a set of vectors          2.3 The span of a set of vectors        January 6 January 10            1\/6  1\/7  1\/8  1\/9  1\/10      2.4 Linear independence         2.5 Matrix transformations         Classes Cancelled (Rain).        January 13 January 17            1\/13  1\/14  1\/15  1\/16  1\/17      Reassessment 1         3.1 Invertibility         3.2 Bases and coordinate systems        January 20 January 24            1\/20  1\/21  1\/22  1\/23  1\/24     Martin Luther King Jr. Day  No Classes        Section 3.4 Determinants         Section 3.4 Determinants  3.5 Subspaces        January 27 January 31            1\/27  1\/28  1\/29  1\/30  1\/31      4.1 An introduction to eigenvalues and eigenvectors         4.2 Finding eigenvalues and eigenvectors         4.3 Diagonalization, similarity, and powers of a matrix        February 3 February 7            2\/3  2\/4  2\/5  2\/6  2\/7      Reassessment 2         Section 6.1: The dot product         Last day to drop courses or resign with W grades    6.2 Orthogonal complements and the matrix transpose        February 10 February 14         2\/10  2\/11  2\/12  2\/13  2\/14      6.2 Orthogonal complements and the matrix transpose          6.3 Orthogonal bases and projections         6.3 Orthogonal bases and projections        February 17 February 21            2\/17  2\/18  2\/19  2\/20  2\/21      6.4 Finding orthogonal bases         Review         Reassessment 3        February 24 February 28            2\/24  2\/25  2\/26  2\/27  2\/28      Review         Final Reassessment      Last Day of Classes            The instructor reserves the right to modify the schedule as needed.    "
+},
+{
+  "id": "schedule-13-1",
+  "level": "2",
+  "url": "schedule.html#schedule-13-1",
+  "type": "Warning",
+  "number": "7.1",
+  "title": "",
+  "body": " The instructor reserves the right to modify the schedule as needed.  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
